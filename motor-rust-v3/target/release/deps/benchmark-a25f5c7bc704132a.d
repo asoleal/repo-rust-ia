bee7f82b8cde8fdded973344767fc84fb6722af4
John Leal <jlealgom@unal.edu.co>
2026-02-27 18:11:56 -0500

    🧹 Limpieza profunda: .gitignore añadido y binarios eliminados

--- motor-rust-v3/target/release/deps/benchmark-a25f5c7bc704132a.d
+++
@@ -1,5 +0,0 @@
-/home/jjlg/repo-rust-ia/motor-rust-v3/target/release/deps/benchmark-a25f5c7bc704132a.d: src/bin/benchmark.rs
-
-/home/jjlg/repo-rust-ia/motor-rust-v3/target/release/deps/benchmark-a25f5c7bc704132a: src/bin/benchmark.rs
-
-src/bin/benchmark.rs:
--- motor-rust-v3/target/release/deps/motor_v3-d99c858bb00da758.d
+++
@@ -1,7 +0,0 @@
-/home/jjlg/repo-rust-ia/motor-rust-v3/target/release/deps/motor_v3-d99c858bb00da758.d: src/lib.rs
-
-/home/jjlg/repo-rust-ia/motor-rust-v3/target/release/deps/libmotor_v3-d99c858bb00da758.rlib: src/lib.rs
-
-/home/jjlg/repo-rust-ia/motor-rust-v3/target/release/deps/libmotor_v3-d99c858bb00da758.rmeta: src/lib.rs
-
-src/lib.rs:
